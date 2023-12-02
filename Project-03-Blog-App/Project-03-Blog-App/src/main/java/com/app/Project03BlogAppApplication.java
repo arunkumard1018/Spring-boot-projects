1f015537500b08baa753e319d26b0f607e4b64e8
ArunKumar_D <arunkumard1018@gmail.com>
2023-12-02 21:10:29 +0530

    strting project-03 Blog-Web-App using Spring boot

---
+++ Project-03-Blog-App/Project-03-Blog-App/src/main/java/com/app/Project03BlogAppApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project03BlogAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project03BlogAppApplication.class, args);
+	}
+
+}

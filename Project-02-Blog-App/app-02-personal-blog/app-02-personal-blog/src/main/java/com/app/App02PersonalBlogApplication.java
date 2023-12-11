e91a3e0ca34c4f654003496c0453f8ebf4a37b6a
ArunKumar_D <arunkumard1018@gmail.com>
2023-12-11 16:43:58 +0530

    re organizing project structer

---
+++ Project-02-Blog-App/app-02-personal-blog/app-02-personal-blog/src/main/java/com/app/App02PersonalBlogApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App02PersonalBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App02PersonalBlogApplication.class, args);
+	}
+
+}
